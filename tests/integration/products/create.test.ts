aed8a0deb7d99a6ac4e23eaaa910196c27283c2a
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-09-19 20:12:41 -0300

    Corrigindo testes do Req 1

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -15,7 +15,25 @@ describe('Testando a rota Products', function () {
     const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
 
     expect(httpResponse.status).to.equal(400);
-    expect(httpResponse.body).to.be.deep.equal({ message: 'Check required fields'});
+    expect(httpResponse.body).to.be.deep.equal({ message: '"name" is required'});
+  });
+
+  it ('Deverá retornar um erro, caso o "name" não seja do tipo string', async function () {
+    const httpRequestBody = productMock.invalidNameType;
+
+    const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
+
+    expect(httpResponse.status).to.equal(422);
+    expect(httpResponse.body).to.be.deep.equal({ message: '"name" must be a string'});
+  });
+
+  it ('Deverá retornar um erro, caso o "name" não possua mais de 2 caracteres', async function () {
+    const httpRequestBody = productMock.invalidNameLength;
+
+    const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
+
+    expect(httpResponse.status).to.equal(422);
+    expect(httpResponse.body).to.be.deep.equal({ message: '"name" length must be at least 3 characters long'});
   });
 
   it ('Deverá retornar um erro, caso não informe o "price"', async function () {
@@ -24,19 +42,28 @@ describe('Testando a rota Products', function () {
     const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
 
     expect(httpResponse.status).to.equal(400);
-    expect(httpResponse.body).to.be.deep.equal({ message: 'Check required fields'});
+    expect(httpResponse.body).to.be.deep.equal({ message: '"price" is required'});
   });
 
-  it ('Deverá retornar um erro, caso não informe o "order"', async function () {
-    const httpRequestBody = productMock.noOrderBody;
+  it ('Deverá retornar um erro, caso o "price" não seja do tipo string', async function () {
+    const httpRequestBody = productMock.invalidPriceType;
 
     const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
 
-    expect(httpResponse.status).to.equal(400);
-    expect(httpResponse.body).to.be.deep.equal({ message: 'Check required fields'});
+    expect(httpResponse.status).to.equal(422);
+    expect(httpResponse.body).to.be.deep.equal({ message: '"price" must be a string'});
   });
 
-  it ('ao receber o request no formato correto, retorne as informações do produto registrado', async function () {
+  it ('Deverá retornar um erro, caso o "price" não possua mais de 2 caracteres', async function () {
+    const httpRequestBody = productMock.invalidPriceLength;
+
+    const httpResponse = await chai.request(app).post('/products').send(httpRequestBody);
+
+    expect(httpResponse.status).to.equal(422);
+    expect(httpResponse.body).to.be.deep.equal({ message: '"price" length must be at least 3 characters long'});
+  });
+
+  it ('Deverá retornar as informações do produto registrado, ao passar pelas validações', async function () {
     const request = productMock.productRequest;
     const response = productMock.productResponse;
     const mockCreate = ProductModel.build({ id: 6, name: "Martelo de Thor", price: "30 peças de ouro",   orderId: 4 });
--- tests/mocks/product.mock.ts
+++ tests/mocks/product.mock.ts
@@ -2,6 +2,27 @@ const noNameBody = { price: '30 peças de ouro', orderId: 4 };
 const noPriceBody = { name: 'Martelo de Thor', orderId: 4 };
 const noOrderBody = { name: 'Martelo de Thor', price: '30 peças de ouro' };
 
+const invalidNameType = {
+  name: 1,
+  price: "30 peças de ouro",
+  orderId: 4
+};
+const invalidNameLength = {
+  name: "M",
+  price: "30 peças de ouro",
+  orderId: 4
+};
+
+const invalidPriceType = {
+  name: "Martelo de Thor",
+  price: 30,
+  orderId: 4
+};
+const invalidPriceLength = {
+  name: "Martelo de Thor",
+  price: "30",
+  orderId: 4
+};
 const productRequest = {
   name: "Martelo de Thor",
   price: "30 peças de ouro",
@@ -18,6 +39,10 @@ export default {
   noNameBody,
   noPriceBody,
   noOrderBody,
+  invalidNameType,
+  invalidNameLength,
+  invalidPriceType,
+  invalidPriceLength,
   productResponse,
   productRequest,
 };
