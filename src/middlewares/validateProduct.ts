ea9a84994c322d76707b3110e98bf3f41b7b1490
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-09-20 14:46:02 -0300

    Requisito 5 corrigido

--- src/middlewares/validateProduct.ts
+++ src/middlewares/validateProduct.ts
@@ -3,14 +3,13 @@ import { NextFunction, Request, Response } from 'express';
 export const validateName = (req: Request, res: Response, next: NextFunction) => {
   const { name } = req.body;
   if (!name) {
-    return { status: 400, data: { message: '"name" is required' },
-    };
+    return res.status(400).json({ message: '"name" is required' });
   }
   if (typeof name !== 'string') {
-    return { status: 422, data: { message: '"name" must be a string' } };
+    return res.status(422).json({ message: '"name" must be a string' });
   }
   if (name.length <= 2) {
-    return { status: 422, data: { message: '"name" length must be at least 3 characters long' } };
+    return res.status(422).json({ message: '"name" length must be at least 3 characters long' });
   }
   next();
 };
@@ -18,14 +17,13 @@ export const validateName = (req: Request, res: Response, next: NextFunction) =>
 export const validatePrice = (req: Request, res: Response, next: NextFunction) => {
   const { price } = req.body;
   if (!price) {
-    return { status: 400, data: { message: '"price" is required' },
-    };
+    return res.status(400).json({ message: '"price" is required' });
   }
   if (typeof price !== 'string') {
-    return { status: 422, data: { message: '"price" must be a string' } };
+    return res.status(422).json({ message: '"price" must be a string' });
   }
   if (price.length <= 2) {
-    return { status: 422, data: { message: '"price" length must be at least 3 characters long' } };
+    return res.status(422).json({ message: '"price" length must be at least 3 characters long' });
   }
   next();
 };
